7741762e66ee2a9597a8b222cc1055f62232c9c9
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-09-19 15:58:59 +0200

    modify the deployment render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -24,9 +24,6 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   # Disable serving static files from the `/public` folder by default since
 # Apache or NGINX already handles this.
-
- 
-config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
