2f6e70098af310ae9a9e7c7da8e6df54fb930cd3
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-09-19 16:36:13 +0200

    modify the deployment render

--- db/migrate/20230912104147_create_entities.rb
+++ db/migrate/20230912104147_create_entities.rb
@@ -4,7 +4,7 @@ class CreateEntities < ActiveRecord::Migration[7.0]
       t.string :name
       t.decimal :amount
       t.belongs_to :author, null: false, foreign_key: { to_table: :users }
-      t.belongs_to :group, null: false, foreign_key: true 
+
       t.timestamps
     end
   end
